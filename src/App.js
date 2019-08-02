75959f5404c03e2c9fb93631262fee63ab7f7494
Saubhik Singh <saubhik.singh01@gmail.com>
2019-08-02 21:45:13 +0530

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import HoemPage from './components/pages/homepage/homepage.component'
+
+function Hats(){
+  return (
+    <div>
+      <h1>Hello Hats</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HoemPage />
+      <Switch>
+        <Route exact path='/' component={HoemPage} />
+        <Route path='/hats' component={Hats} />
+      </Switch>
+
     </div>
   );
 }
