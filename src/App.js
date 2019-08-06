0342ff4ea332c16e381e6ba31c851459903f173a
saubhik <saubhik.singh01@gmail.com>
2019-08-06 18:17:07 +0530

    status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
 import HoemPage from './components/pages/homepage/homepage.component'
 
 function App() {
   return (
     <div>
-      <HoemPage />
+      <Switch>
+        <Route exact path='/' component={HoemPage} />>
+      </Switch>
+      
     </div>
   );
 }
