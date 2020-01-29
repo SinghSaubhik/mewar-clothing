689344e5da6be4639e8855cf07478c77ee3ec43f
Saubhik Singh <saubhik.singh01@gmail.com>
2020-01-29 15:21:28 +0530

    Added build folder

---
+++ build/precache-manifest.cdec947818f2522de9216feef603445e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d896a737e9ad082ea3d7289313909f89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6785078ae06bc06bb10d",
+    "url": "/static/css/main.3737819a.chunk.css"
+  },
+  {
+    "revision": "6cdc514e4cb116b94617",
+    "url": "/static/js/2.f6cae820.chunk.js"
+  },
+  {
+    "revision": "1b69dfa0affd7bc516d5023038b5e2b2",
+    "url": "/static/js/2.f6cae820.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6785078ae06bc06bb10d",
+    "url": "/static/js/main.1518f2c4.chunk.js"
+  },
+  {
+    "revision": "cd436190aaa31c20de01",
+    "url": "/static/js/runtime-main.6eedf2f7.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
